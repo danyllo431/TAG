f4367ee1f698d4f4b6620e92e912f59de2d43103
danyllo <danyllopontocom@hotmail.com>
2015-06-22 14:18:33 -0300

    menu
    criação de menu

---
+++ Menu.php
@@ -0,0 +1,25 @@
+<div id='container'>
+      <nav class="menu">
+        <div class="submenu">
+          <ul>
+          <a href='#' class="logo"></a>
+          
+              <li><a href="#">Início</a></li>
+              <li><a href="#">Gases</a></li>
+              <li><a href="#">Epoxi</a></li>
+              <li><a href="#">Serviços</a></li>
+              <li><a href="#">Fotos</a></li>
+               </ul>
+          
+           </div>
+           <div class="buscar">
+          <form name="frmBusca" method="post" action="#" class="search">
+              <input type="search" name="palavra" class="input_pesquisa" placeholder="Faça sua busca">
+              <input type="submit" value="Buscar">
+              </form>   
+         </nav>
+          
+
+    </div><!--fim div container-->
+   
+          </div>
---
+++ index.php
@@ -0,0 +1,37 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>TAG Soluções</title>
+  <meta http-equiv="content-type" content="text/html;charset=utf-8" />
+  <link rel="stylesheet" href="style.css" media="all" />
+  <link rel="stylesheet" type="text/css" href="Menu.css">
+  <link rel="stylesheet" type="text/css" href="CSS/Rodape.css">
+  <link rel="shortcult icon" type="image/x-icon" href="favicon.png">
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"/>
+</script>
+<script>
+
+
+    </script>
+</head>
+<body >
+  <?php include('Menu.php'); ?>
+</br>
+</br>
+</br>
+</br>
+</br>
+
+ 
+  <div id='corpo'>
+   
+  <img src="2_1_1_cylinders_1400x298.jpg" width="1410" height="550">  
+ 
+      
+</div>
+<?php include('rodape.php'); ?>
+</body>
+</html>
