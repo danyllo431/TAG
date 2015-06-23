2567c1a6fd5980e4a0c37b552bf3d3d1d941fb55
danyllo <danyllopontocom@hotmail.com>
2015-06-23 14:23:20 -0300

    tentando slides

--- index.php
+++ index.php
@@ -6,32 +6,39 @@
 <head>
   <title>TAG Soluções</title>
   <meta http-equiv="content-type" content="text/html;charset=utf-8" />
-  <link rel="stylesheet" href="style.css" media="all" />
-  <link rel="stylesheet" type="text/css" href="Menu.css">
-  <link rel="stylesheet" type="text/css" href="CSS/Rodape.css">
+    <link rel="stylesheet" type="text/css" href="footer.css">
   <link rel="stylesheet" href="styles.css">
    <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
    <script src="script.js"></script>
+   <script type="text/javascript"src="Jquery.js" ></script>
+   <script type="text/javascript"src="jcycle.js" ></script>
   <link rel="shortcult icon" type="image/x-icon" href="favicon.png">
   <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"/>
 </script>
-<script>
+<script type="text/javascript">
 
+$(function(){
 
+  $('#slide ul').cycle({
+    fx:'fade',
+    speed:2000,
+    timeout: 5000,
+  })
+}) 
     </script>
 </head>
 <body >
   <?php include('Menu.php'); ?>
-</br>
-
-
- 
-  <div id='corpo'>
-   
-  <img src="2_1_1_cylinders_1400x298.jpg" width="1410" height="550">  
- 
-      
+<div id="slide">
+   <ul>
+    <li><img src="imagen1.jpg" alt="" title="" width="1000" height="620"></li>
+   </ul>
+      <ul>
+    <li><img src="imagen2.jpg" alt="" title="" width="1000" height="620"></li>
+   </ul>
+   <ul>
+    <li><img src="imagen3.jpg" alt="" title="" width="1000" height="620"></li>
+   </ul>
 </div>
-<?php include('rodape.php'); ?>
 </body>
 </html>
