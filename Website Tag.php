2e757c88377205e79c9ebd6a27963ecd4c63fdcf
danyllo <danyllopontocom@hotmail.com>
2015-06-25 19:04:27 -0300

    defeinição

---
+++ Website Tag.php
@@ -0,0 +1,38 @@
+Visita 1 definições de nomeclaturas e quantidades de paginas cores gradientes que vão ser ultilizado nos Web site; dia 25-06-2015 
+
+visita 2 definição de layout da paginação recolhimento de arquivos desejados para amostra no site escolha do da base de dados a gosto do cliente;
+
+Visita 3 apresentação de esbolço sujeito a ajustes pelo cliente;
+
+visita 4 conclusão e hospedagem do web site e testes finais.
+
+
+Cor azul do menu ok(x)
+divisão dos gases industriais medidicinais especiais
+equipamentos pagina 2 
+revestimento epoxi pagina diferente pagina3
+
+
+parceiros {
+	alcoa
+	fronpet
+	coca cola
+	hebron
+	linde
+	copergas
+	sao braz
+	sococo
+	acelormital
+	bunge
+	white martins
+	swagelon
+	rotarex
+	hoke
+	proenerey
+}
+tagsi.com.br
+senha = calife02
+contatos
+phone - 81 3077-8313| 81 3040-8313
+email - contato@tagsi.com.br
+skype - tag.solucoes.industriais
