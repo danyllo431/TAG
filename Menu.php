2692e7c17c3dc7b490e3f099757401b654c42748
victorbernardo <victorbernardo_@hotmail.com>
2015-07-01 12:00:00 -0300

    pagina acetileno

--- Menu.php
+++ Menu.php
@@ -1,58 +1,58 @@
          <div id='cssmenu'>
-   <ul>
-          <a href='index.php' class="logo"></a>
-          <li class='active'><a href='index.php'>Empresa</a></li>
-          <li class='has-sub'><a href='#'>Gases</a>
-          <ul>
+           <ul>
+            <a href='index.php' class="logo"></a>
+            <li class='active'><a href='index.php'>Empresa</a></li>
+            <li class='has-sub'><a href='#'>Gases</a>
+              <ul>
                <li><a href='#'>industriais</a>
-               <ul>
-               <li><a href='oxigenio.php'>Oxigênio</a></li>
-               <li><a href='#'>Acetileno</a></li>
-               <li><a href='#'>Argônio</a></li>
-               <li><a href='#'>Nitrogênio</a></li>
-               <li><a href='#'>Dioxido de Carbono</a></li>
-               <li><a href='#'>Misturas para Solda</a></li>
-               <li><a href='#'>Hidrogênio</a></li>
-               <li><a href='#'>Nitrogênio Liquido</a></li>
-            </ul>
-          </li>
-                   <li><a href='#'>Especiais</a>
-                                <ul>
-               <li><a href='#'>Acetilento 2.7</a></li>
-               <li><a href='#'>AR Sintetico</a></li>
-               <li><a href='#'>Argônio(Ar)5.0</a></li>
-               <li><a href='#'>Dióxido de Carbono</a></li>
-               <li><a href='#'>Etileno 2.5</a></li>
-               <li><a href='#'>Hélio(He)5.0</a></li>
-               <li><a href='#'>Hidrogênio 4.5</a></li>
-               <li><a href='#'>Nitrogênio 5.0</a></li>
-               <li><a href='#'>Óxido nitroso</a></li>
-               <li><a href='#'>Oxigenio</a></li>
-               <li><a href='#'>Óxido nitroso 2O</a></li>
+                 <ul>
+                   <li><a href='oxigenio.php'>Oxigênio</a></li>
+                   <li><a href='acetileno.php'>Acetileno</a></li>
+                   <li><a href='#'>Argônio</a></li>
+                   <li><a href='#'>Nitrogênio</a></li>
+                   <li><a href='#'>Dioxido de Carbono</a></li>
+                   <li><a href='#'>Misturas para Solda</a></li>
+                   <li><a href='#'>Hidrogênio</a></li>
+                   <li><a href='#'>Nitrogênio Liquido</a></li>
+                 </ul>
+               </li>
+               <li><a href='#'>Especiais</a>
+                <ul>
+                 <li><a href='#'>Acetilento 2.7</a></li>
+                 <li><a href='#'>AR Sintetico</a></li>
+                 <li><a href='#'>Argônio(Ar)5.0</a></li>
+                 <li><a href='#'>Dióxido de Carbono</a></li>
+                 <li><a href='#'>Etileno 2.5</a></li>
+                 <li><a href='#'>Hélio(He)5.0</a></li>
+                 <li><a href='#'>Hidrogênio 4.5</a></li>
+                 <li><a href='#'>Nitrogênio 5.0</a></li>
+                 <li><a href='#'>Óxido nitroso</a></li>
+                 <li><a href='#'>Oxigenio</a></li>
+                 <li><a href='#'>Óxido nitroso 2O</a></li>
 
-            </ul>               
-            </ul></li>
-            <li class='has-sub'><a href='#'>Equipamentos</a>
+               </ul>               
+             </ul></li>
+             <li class='has-sub'><a href='#'>Equipamentos</a>
               <ul>
                <li class='has-sub'><a href='#'>Valvulas</a>
-               <ul>
+                 <ul>
                    <li class='has-sub'><a href='#'>Reguladora</a>
 
-                  <ul>
-                   <li><a href='#'>De Pressão</a>
-                    <li><a href='#'>Corta Chama</a>
+                    <ul>
+                     <li><a href='#'>De Pressão</a>
+                      <li><a href='#'>Corta Chama</a>
                       </ul>
 
-                   </li>
-               </ul>
-               </li>
-               <li><a href='#'>Maçaricos</a></li>
-               <li><a href='#'>Bicos</a></li>
-               </ul>
+                    </li>
+                  </ul>
+                </li>
+                <li><a href='#'>Maçaricos</a></li>
+                <li><a href='#'>Bicos</a></li>
+              </ul>
             </li>
-          <li><a href='#'> revestimento Epoxi</a></li>
-          <li><a href='instalacoes.php'>INSTALAÇOES CENTRALIZADAS</a></li>
-               <li><a href='#'>Parceiros</a></li>
-          <li><a href='fale-conosco.php'>Fale conosco</a></li>
-</ul>
-</div
+            <li><a href='#'> revestimento Epoxi</a></li>
+            <li><a href='instalacoes.php'>INSTALAÇOES CENTRALIZADAS</a></li>
+            <li><a href='#'>Parceiros</a></li>
+            <li><a href='fale-conosco.php'>Fale conosco</a></li>
+          </ul>
+          </div>
